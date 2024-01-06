f8197559ba875da883bd792eef4c0f6e7ebe9747
Abanoub Girgis <abanoubsamir0004@gmail.com>
2024-01-06 16:47:30 +0000

    solve Issues

--- models/Staging/jaffle_shop/stg_jaffle_shop__orders_2.sql
+++ models/Staging/jaffle_shop/stg_jaffle_shop__orders_2.sql
@@ -10,13 +10,12 @@ transformed as (
 
         id as order_id,
         user_id as customer_id,
-        order_date,
+        order_date as order_placed_at,
         status as order_status,
         case 
             when order_status not in ('returned','return_pending') 
             then order_date 
-        end as valid_order_date,
-
+        end as valid_order_date
     from  source
 )
 
