1e71198317a84b03fae4a929c2a505d4607e8544
Abanoub Girgis <abanoubsamir0004@gmail.com>
2024-01-16 09:45:39 +0000

    change query name

---
+++ models/queries/ABANOUB_query_2.sql
@@ -0,0 +1,31 @@
+{# Find the neighborhood with the most total units.#}
+
+WITH 
+
+    FACT_SALES AS (
+        SELECT * FROM {{ ref('ABANOUB_FACT_SALES') }}
+    ),
+
+    DIM_LOCATION  AS (
+        SELECT * FROM {{ ref('STG_ABANOUB_DIM_LOCATION') }}
+    ),
+
+    TOTAL_UNITS_BY_NEIGHBORHOOD AS (
+        SELECT
+            L.NEIGHBORHOOD,
+            SUM(F.TOTAL_UNITS) AS TOTAL_UNITS
+        FROM
+            FACT_SALES F
+        INNER JOIN
+            DIM_LOCATION L ON F.LOCATION_ID = L.LOCATION_ID
+        WHERE F.TOTAL_UNITS IS NOT NULL
+        GROUP BY
+            L.NEIGHBORHOOD
+    )
+
+SELECT *
+FROM
+    TOTAL_UNITS_BY_NEIGHBORHOOD 
+ORDER BY
+    TOTAL_UNITS DESC
+LIMIT 1
